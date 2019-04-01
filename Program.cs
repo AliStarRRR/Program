cc761a2b0bdaa35c9f37000c92154a59e162b11f
AliStarRRR <43142575+AliStarRRR@users.noreply.github.com>
2019-04-02 00:13:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Homework
+{
+    class IsCollinear
+    {
+        static void Main()
+        {
+            double x1, y1, x2, y2;
+
+            Console.WriteLine("Введите координаты первого вектора (x1 и y1):");
+            x1 = double.Parse(Console.ReadLine());
+            y1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты второго вектора (x2 и y2):");
+            x2 = double.Parse(Console.ReadLine());
+            y2 = double.Parse(Console.ReadLine());
+
+        
+            if ((x1/x2) == (y1/y2))
+            {
+                Console.WriteLine("Векторы коллинеарны");
+            }
+            else
+            {
+                Console.WriteLine("Векторы не коллинеарны");
+            }
+            Console.ReadKey();
+        }
+    }
+}
